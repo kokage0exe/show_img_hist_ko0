61398a61713cf1608e5e27f74ac9a5e7db0d65f4
Shousuke Kawaguchi <shou0317.akn1029@outlook.jp>
2023-06-21 01:42:34 +0900

    change original color

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 from skimage.io import imread
 
 
@@ -17,10 +18,18 @@ st.image(im, caption='image from wikimedia commons',
 hist_red, _ = np.histogram(im[:, :, 0], bins=64)
 hist_green, _ = np.histogram(im[:, :, 1], bins=64)
 hist_blue, _ = np.histogram(im[:, :, 2], bins=64)
-hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
+# hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
 
-df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
-st.bar_chart(df_hist)
+# df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
+# st.bar_chart(df_hist)
+
+colors = ['red', 'green', 'blue']
+labels = ['R', 'G', 'B']
+hist_data = [hist_red, hist_green, hist_blue]
+
+fig, ax = plt.subplots()
+for i, data in enumerate(hist_data):
+    ax.bar(np.arange(len(data)), data, color=colors[i], label=labels[i])
 
 
 # choose one color
@@ -28,11 +37,17 @@ color = st.radio(
     "choose R, G, or B",
     ('R', 'G', 'B'))
 if color == 'R':
-    df_hist = pd.DataFrame(hist_red)
-    st.bar_chart(df_hist)
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(len(hist_red)), hist_red, color='red')
+    ax.set_title('Histogram of Red Color')
+    st.pyplot(fig)
 if color == 'G':
-    df_hist = pd.DataFrame(hist_green)
-    st.bar_chart(df_hist)
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(len(hist_green)), hist_green, color='green')
+    ax.set_title('Histogram of Green Color')
+    st.pyplot(fig)
 if color == 'B':
-    df_hist = pd.DataFrame(hist_blue)
-    st.bar_chart(df_hist)
+    fig, ax = plt.subplots()
+    ax.bar(np.arange(len(hist_blue)), hist_blue, color='blue')
+    ax.set_title('Histogram of Blue Color')
+    st.pyplot(fig)
